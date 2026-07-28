e594956915cb4941a07941e2211f36b2cd4d3763
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-07-28 08:53:07 +0200

    feat: distinct by patient and disease

--- src/main/java/dev/dnpm/zpmdashboard/ZpmDashboardService.kt
+++ src/main/java/dev/dnpm/zpmdashboard/ZpmDashboardService.kt
@@ -79,8 +79,10 @@ class ZpmDashboardService(private val onkostarApi: IOnkostarApi, dataSource: Dat
 
     fun findPrimaerfaelleCaseId(year: Int): List<CaseId> {
         val sql =
-            """SELECT DISTINCT pat.patienten_id, pat.guid AS pat_guid, p.guid AS proc_guid FROM dk_zpm_auswertungen zpm
+            """SELECT DISTINCT pat.patienten_id, pat.guid AS pat_guid, p.guid AS proc_guid, e.guid AS e_guid FROM dk_zpm_auswertungen zpm
             JOIN prozedur p ON (zpm.id = p.id)
+            JOIN erkrankung_prozedur ep ON (p.id = ep.prozedur_id) 
+            JOIN erkrankung e ON (ep.erkrankung_id = e.id)
             JOIN patient pat ON (p.patient_id = pat.id)
             WHERE YEAR(zaehlzeitpunkt) = :year AND p.geloescht <> 1 AND zpm.primaerfall = 1 AND pat.nachname <> 'Momentum'
             ORDER BY zaehlzeitpunkt, pat.patienten_id;
@@ -93,9 +95,9 @@ class ZpmDashboardService(private val onkostarApi: IOnkostarApi, dataSource: Dat
         return jdbcTemplate.query(sql, params, ResultSetExtractor { rs: ResultSet? ->
             val caseIds = mutableListOf<CaseId>()
             while (rs!!.next()) {
-                caseIds.add(CaseId(rs.getString("patienten_id"), rs.getString("pat_guid"), rs.getString("proc_guid")))
+                caseIds.add(CaseId(rs.getString("patienten_id"), rs.getString("pat_guid"), rs.getString("proc_guid"), rs.getString("e_guid")))
             }
-            return@ResultSetExtractor caseIds.distinctBy { it.patientGuid }
+            return@ResultSetExtractor caseIds.distinctBy { it.patientGuid + it.erkrankungGuid }
         })
     }
 
@@ -200,6 +202,7 @@ class ZpmDashboardService(private val onkostarApi: IOnkostarApi, dataSource: Dat
         val pid: String,
         val patientGuid: String,
         val procedureGuid: String,
+        val erkrankungGuid: String
     )
 
     data class Case(

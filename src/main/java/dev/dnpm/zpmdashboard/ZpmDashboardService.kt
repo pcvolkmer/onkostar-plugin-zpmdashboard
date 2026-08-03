1db643d2ab5543a3264fdc0d52ee212dc977a6f8
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-08-03 14:07:18 +0200

    feat: additional information in XLS file

--- src/main/java/dev/dnpm/zpmdashboard/ZpmDashboardService.kt
+++ src/main/java/dev/dnpm/zpmdashboard/ZpmDashboardService.kt
@@ -188,7 +188,7 @@ class ZpmDashboardService(private val onkostarApi: IOnkostarApi, dataSource: Dat
         cellStyle.borderLeft = BorderStyle.THIN
         cellStyle.borderRight = BorderStyle.THIN
 
-        val headers = listOf("PID", "ICD10", "Zählzeitpunkt", "Consent-Datum", "Consentzustimmung", "Warnung?", "Kein Primärfall?", "Kein MolGen?", "Keine Erkrankung?")
+        val headers = listOf("PID", "ICD10", "intern/extern", "Studie", "off-label", "Zählzeitpunkt", "Consent-Datum", "Consentzustimmung", "Warnung?", "Kein Primärfall?", "Kein MolGen?", "Keine Erkrankung?")
         val headRow = sheet.createRow(0)
         headers.forEachIndexed { idx, value ->
             val cell = headRow.createCell(idx)
@@ -211,31 +211,43 @@ class ZpmDashboardService(private val onkostarApi: IOnkostarApi, dataSource: Dat
                 icd10Cell.setCellValue(case.icd.orEmpty())
                 icd10Cell.cellStyle = cellStyle
 
-                val zZeitpunktCell  = row.createCell(2)
+                val internExternColumn  = row.createCell(2)
+                internExternColumn.setCellValue(if (case.internextern == "E") { "extern" } else { "intern" })
+                internExternColumn.cellStyle = cellStyle
+
+                val studieColumn  = row.createCell(3)
+                studieColumn.setCellValue(if (case.studie) { "Ja" } else { "Nein" })
+                studieColumn.cellStyle = cellStyle
+
+                val offLabelColumn  = row.createCell(4)
+                offLabelColumn.setCellValue(if (case.offlabel) { "Ja" } else { "Nein" })
+                offLabelColumn.cellStyle = cellStyle
+
+                val zZeitpunktCell  = row.createCell(5)
                 zZeitpunktCell.setCellValue(case.zaehlzeitpunkt.orEmpty())
                 zZeitpunktCell.cellStyle = cellStyle
 
-                val consentCell = row.createCell(3)
+                val consentCell = row.createCell(6)
                 consentCell.setCellValue(case.consent.datum.orEmpty())
                 consentCell.cellStyle = cellStyle
 
-                val consentAcceptedCell = row.createCell(4)
+                val consentAcceptedCell = row.createCell(7)
                 consentAcceptedCell.setCellValue(if (case.consent.zustimmung) { "Ja" } else { "Nein" })
                 consentAcceptedCell.cellStyle = cellStyle
 
-                val warningCell = row.createCell(5)
+                val warningCell = row.createCell(8)
                 warningCell.setCellValue(if (case.warnings) { "Ja" } else { "Nein" })
                 warningCell.cellStyle = cellStyle
 
-                val keinPF = row.createCell(6)
+                val keinPF = row.createCell(9)
                 keinPF.setCellValue(if (case.warningDetails?.invalidPrimaerfall == true) { "Ja" } else { "Nein" })
                 keinPF.cellStyle = cellStyle
 
-                val noMolGen = row.createCell(7)
+                val noMolGen = row.createCell(10)
                 noMolGen.setCellValue(if (case.warningDetails?.noMolgen == true) { "Ja" } else { "Nein" })
                 noMolGen.cellStyle = cellStyle
 
-                val noDisease = row.createCell(8)
+                val noDisease = row.createCell(11)
                 noDisease.setCellValue(if (case.warningDetails?.noDisease == true) { "Ja" } else { "Nein" })
                 noDisease.cellStyle = cellStyle
             }

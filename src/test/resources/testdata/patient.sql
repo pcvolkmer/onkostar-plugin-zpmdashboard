98ab70b05f626c7b98296b5cb7b5839f28b7f4a0
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-08-03 14:25:22 +0200

    test: add tests involving a test database (#48)

---
+++ src/test/resources/testdata/patient.sql
@@ -0,0 +1,5 @@
+/* Verfügbare Testpatienten */
+
+insert into patient (id, vorname, nachname, guid) values (1, 'Patrick', 'Tester', '00000000-0000-0000-0000-000000000001');
+insert into patient (id, vorname, nachname, guid) values (2, 'Patricia', 'Tester', '00000000-0000-0000-0000-000000000002');
+insert into patient (id, vorname, nachname, guid) values (3, 'Paola', 'Tester', '00000000-0000-0000-0000-000000000003');

315d8fef37a2c1b9895a0e9237b58d51d4ace952
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-09-10 13:23:19 +0200

    feat: find matching einsendummer in different formats

---
+++ src/test/java/dev/dnpm/zpmdashboard/EinsendenummerTest.kt
@@ -0,0 +1,31 @@
+package dev.dnpm.zpmdashboard
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class EinsendenummerTest {
+
+    @Test
+    fun test() {
+        assertThat(
+            Einsendenummer("A/20/123").matches(Einsendenummer("A/20/123"))
+        ).isTrue()
+
+        assertThat(
+            Einsendenummer("A123-20").matches(Einsendenummer("A/20/123"))
+        ).isTrue()
+
+        assertThat(
+            Einsendenummer("A/20/123").matches(Einsendenummer("A0123-20"))
+        ).isTrue()
+
+        assertThat(
+            Einsendenummer("A/20/0123").matches(Einsendenummer("A/20/123"))
+        ).isTrue()
+
+        assertThat(
+            Einsendenummer("A/2020/123").matches(Einsendenummer("A/20/123"))
+        ).isTrue()
+    }
+
+}

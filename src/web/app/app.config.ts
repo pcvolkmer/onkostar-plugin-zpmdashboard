f9c5689e42af4ef837219b419902720ea79cc12e
Paul-Christian Volkmer <code@pcvolkmer.de>
2026-09-23 01:45:08 +0200

    feat: add charts (#77)

--- src/web/app/app.config.ts
+++ src/web/app/app.config.ts
@@ -5,10 +5,28 @@ import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {MockBackendInterceptor} from '../mock-backend.interceptor';
 import {environment} from '../environment';
+import {provideEchartsCore} from "ngx-echarts";
+import * as echarts from 'echarts/core';
+import { PieChart } from 'echarts/charts';
+import {
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent
+} from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([
+    TitleComponent,
+    PieChart,
+    TooltipComponent,
+    LegendComponent,
+    CanvasRenderer
+]);
 
 const providers: any[] = [
   provideBrowserGlobalErrorListeners(),
-  provideRouter(routes)
+  provideRouter(routes),
+  provideEchartsCore({ echarts })
 ];
 
 if (environment.useMockApi) {
